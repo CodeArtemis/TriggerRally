447b681a1f9940300b87494f6cd346b56228361d
Jasmine Kent <jk@codeartemis.com>
2012-06-19 10:50:54 +0200

    hmm

--- src/drive.js
+++ src/drive.js
@@ -177,12 +177,12 @@ function init() {
 
   //
 
-  var path = "/a/textures/miramar-512/miramar_";
+  var path = "/a/textures/Teide-1024/";
   var format = '.jpg';
   var urls = [
-    path + 'rt' + format, path + 'lf' + format,
-    path + 'ft' + format, path + 'bk' + format,
-    path + 'up' + format, path + 'dn' + format
+    path + 'posx' + format, path + 'negx' + format,
+    path + 'posy' + format, path + 'negy' + format,
+    path + 'posz' + format, path + 'negz' + format
   ];
 
   textureCube = THREE.ImageUtils.loadTextureCube(urls);
