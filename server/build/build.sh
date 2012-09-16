b1899751e1e0dbe907bab2eee4e7274892fde9ca
Jasmine Kent <jk@codeartemis.com>
2012-09-16 14:35:52 +0200

    make build script run from anywhere

--- server/build/build.sh
+++ server/build/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$( dirname "$0" )"
+
 node ../node_modules/requirejs/bin/r.js -o drive.build.js
 mkdir -p ../public/build-v2
 cp ../public/build-out/drive-main.js ../public/build-v2/
