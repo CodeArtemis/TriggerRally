4d91fd3d2fb2f813f353717155197a1363d45f4a
Jasmine Kent <jk@codeartemis.com>
2012-12-18 14:58:47 +0100

    v3

--- server/build/build.sh
+++ server/build/build.sh
@@ -3,6 +3,6 @@
 cd "$( dirname "$0" )"
 
 node ../node_modules/requirejs/bin/r.js -o drive.build.js
-mkdir -p ../public/build-v2
-cp ../public/build-out/drive-main.js ../public/build-v2/
+mkdir -p ../public/build-v3
+cp ../public/build-out/drive-main.js ../public/build-v3/
 rm -r ../public/build-out
