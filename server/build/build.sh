d48a5ed2d4a44ce8673022d8572b3e59f0662d56
Jasmine Kent <jk@codeartemis.com>
2013-01-09 14:51:49 +0000

    fixes

--- server/build/build.sh
+++ server/build/build.sh
@@ -2,9 +2,9 @@
 
 cd "$( dirname "$0" )"
 
-node ../node_modules/requirejs/bin/r.js -o drive.build.js
-node ../node_modules/requirejs/bin/r.js -o editor.build.js
 mkdir -p ../public/build-v3
+node ../node_modules/requirejs/bin/r.js -o drive.build.js
 cp ../public/build-out/drive-main.js ../public/build-v3/
+node ../node_modules/requirejs/bin/r.js -o editor.build.js
 cp ../public/build-out/editor/editor-main.js ../public/build-v3/
 rm -r ../public/build-out
